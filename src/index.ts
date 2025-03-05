223a81dddf3158ec04dbf2225fa243fb5659fe60
Ante Budimir <developer@antebudimir.com>
2025-03-05 20:55:58 +0200

    chore 🔨: add .npmignore to exclude development files from npm package

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import customOrderRule from './css-rules/custom-order/rule-definition.js';
 export const vanillaExtract = {
   meta: {
     name: '@antebudimir/eslint-plugin-vanilla-extract',
-    version: '1.1.1',
+    version: '1.1.2',
   },
   rules: {
     'alphabetical-order': alphabeticalOrderRule,
