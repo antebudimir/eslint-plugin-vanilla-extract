44eeb7be6df7f82550ed4ef4c7ebcc0b83e3bf69
Ante Budimir <developer@antebudimir.com>
2025-03-09 18:36:10 +0200

    chore 🔧: add GitHub Action to create releases from tags

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import customOrderRule from './css-rules/custom-order/rule-definition.js';
 export const vanillaExtract = {
   meta: {
     name: '@antebudimir/eslint-plugin-vanilla-extract',
-    version: '1.4.1',
+    version: '1.4.2',
   },
   rules: {
     'alphabetical-order': alphabeticalOrderRule,
